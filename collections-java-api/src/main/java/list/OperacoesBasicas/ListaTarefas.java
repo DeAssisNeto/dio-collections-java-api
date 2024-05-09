14b81b7a23e4f1cfac5f9505b2c66fd5e21e4c7e
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-05-09 13:02:31 -0300

    feat: desafio ListaTarefas completo.

---
+++ collections-java-api/src/main/java/list/OperacoesBasicas/ListaTarefas.java
@@ -0,0 +1,50 @@
+package main.java.list.OperacoesBasicas;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListaTarefas {
+    private List<Tarefa> tarefaList;
+
+
+    public void adicionarTarefa(String descricao){
+        tarefaList.add(new Tarefa(descricao));
+    }
+
+    public void removerTarefa(String descricao){
+        List<Tarefa> removerTarefas = new ArrayList<>();
+        for (Tarefa tarefa: tarefaList){
+            if (tarefa.getDescricao().equals(descricao))
+                removerTarefas.add(tarefa);
+        }
+        tarefaList.removeAll(removerTarefas);
+    }
+
+    public int obterNumeroTotalTarefas() {
+        return tarefaList.size();
+    }
+
+    public void obterDescricoesTarefas() {
+        for (Tarefa tarefa: tarefaList) {
+            System.out.println(tarefa.getDescricao());
+        }
+    }
+    public ListaTarefas() {
+        this.tarefaList = new ArrayList<>();
+    }
+
+    public static void main(String[] args) {
+        ListaTarefas listaTarefas = new ListaTarefas();
+        System.out.println(listaTarefas.obterNumeroTotalTarefas());
+        listaTarefas.adicionarTarefa("Tarefa 0");
+        listaTarefas.adicionarTarefa("Tarefa 1");
+        listaTarefas.adicionarTarefa("Tarefa 2");
+        System.out.println(listaTarefas.obterNumeroTotalTarefas());
+        listaTarefas.obterDescricoesTarefas();
+        listaTarefas.removerTarefa("Tarefa 0");
+        System.out.println(listaTarefas.obterNumeroTotalTarefas());
+        listaTarefas.obterDescricoesTarefas();
+
+    }
+
+}
