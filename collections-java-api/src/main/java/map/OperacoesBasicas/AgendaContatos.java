ce55d3e0238ee39bc03a7c02e1ba73bb3b1a05d5
Francisco de Assis <francisco.pereira1713@gmail.com>
2024-05-11 12:10:39 -0300

    feat: desafio AgendaContatos completo.

---
+++ collections-java-api/src/main/java/map/OperacoesBasicas/AgendaContatos.java
@@ -0,0 +1,48 @@
+package main.java.map.OperacoesBasicas;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+    private Map<String, Integer> contatos;
+
+    public AgendaContatos() {
+        this.contatos = new HashMap<>();
+    }
+
+    @Override
+    public String toString() {
+        return "AgendaContatos{" +
+                "contatos=" + contatos +
+                '}';
+    }
+
+    public void adicionarContato(String nome, Integer telefone){
+        contatos.put(nome, telefone);
+    }
+
+    public void removerContato(String nome){
+        if (!contatos.isEmpty()) contatos.remove(nome);
+
+    }
+
+    public void exibirContatos(){
+        System.out.println(contatos);
+    }
+
+    public Integer pesquisarPorNome(String nome){
+        Integer numeroContato = contatos.get(nome);
+        if (!contatos.isEmpty()) return numeroContato;
+        return numeroContato;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Tamiris", 1234);
+        System.out.println(agendaContatos.pesquisarPorNome("Tamiris"));
+        agendaContatos.removerContato("Tamiris");
+        agendaContatos.removerContato("Tamiris");
+        agendaContatos.exibirContatos();
+    }
+}
